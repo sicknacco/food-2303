84acf0ac560232fcf4bbb934bbcfaaf37386dcdc
Nick Sacco <nicksacco10@gmail.com>
2023-07-21 09:59:57 -0700

    fin

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -4,10 +4,12 @@ class FoodsController < ApplicationController
       faraday.headers["X-API-KEY"] = ENV["X-API-KEY"]
     end
 
-    response = conn.get("/foods/search?query=sweet%20potato&dataType=&pageSize=10&sortOrder=asc")
+## Cannot find the sweet potato end point no matter what I try. End points taken straight from the docs . . .
+## I get a response, but always says the item can't be found
+    response = conn.get("foods/search?query=sweet%20potato&dataType=&pageSize=10&sortOrder=asc")
     json = JSON.parse(response.body, symbolize_names: true)
-    require 'pry'; binding.pry
-    @food = json[:results].map do |food_data|
+    # require 'pry'; binding.pry
+    @food = json[:foods].map do |food_data|
       Food.new(food_data)
     end
   end
