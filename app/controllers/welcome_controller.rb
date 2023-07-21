159566281364b55d5bec7c8c1c15cb22be6b9823
Nick Sacco <nicksacco10@gmail.com>
2023-07-21 09:11:10 -0700

    finally get some tests written

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,14 @@
 class WelcomeController < ApplicationController
   def index
+    # conn = Faraday.new(url: "https://api.nal.usda.gov") do |faraday|
+    #   faraday.headers["X-API-KEY"] = ENV["PROPUBLICA_API_KEY"]
+    # end
+
+    # response = conn.get("/congress/v1/members/house/#{state}/current.json")
+
+    # json = JSON.parse(response.body, symbolize_names: true)
+    # @members = json[:results].map do |member_data|
+    #   Member.new(member_data)
+    # end
   end
 end
---
+++ spec/features/welcome/index_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe "Welcome Page Search", type: :feature do
+  describe "visiting root path '/" do
+    it "has a search form" do
+      visit '/'
+      
+      fill_in "q", with: "sweet potatoes"
+      click_button "Search"
+
+      expect(current_path).to eq(foods_path)
+      expect(page).to have_content("Total results: > 30,000")
+      expect(page).to have_content("Food with Sweet Potato", count: 10)
+      expect(page).to have_content("GTIN/UPC code:")
+      expect(page).to have_content("Description:")
+      expect(page).to have_content("Brand Owner:")
+      expect(page).to have_content("Ingredients:")
+    end
+  end
+end
